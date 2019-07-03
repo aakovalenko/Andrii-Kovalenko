f5834629f2d60fd45abef038146f37c6a9ad06f3
Andrii <kaa89pl@gmail.com>
2019-07-03 21:09:13 +0300

    home work 1

---
+++ hw1/app.js
@@ -0,0 +1,37 @@
+/*1 */
+
+
+function multiply() {
+
+}
+
+multiply();
+
+/* 2 */
+
+function reverseString(str) {
+	let reverseString = '', i;
+
+	for (i = str.lenght -1 ; i >= 0 ; i--){
+		reverseString += str(i);
+	}
+	return reverseString;
+	
+}
+alert(reverseString('test'));
+
+/* 3 */
+
+/*function getCodeStringFromText(str) {
+
+var uniString = '';
+	for (i = 0; i < str.lenght; i++){
+		uniString += str.charCodeAt(i) . ' ';
+	}
+	return uniString;
+	//console.log(str.charCodeAt(0));
+}
+
+getCodeStringFromText('hello');*/
+
+/* 4 */
