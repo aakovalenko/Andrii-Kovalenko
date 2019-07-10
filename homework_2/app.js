57d2aefecdb9850896e40175f0ba447085160b69
andrii <kaa89pl@gmail.com>
2019-07-10 15:46:56 +0300

    arrow function

--- homework_2/app.js
+++ homework_2/app.js
@@ -10,18 +10,45 @@
 // Name: Google 
 // Partners: Microsoft Facebook
 
-const organisation = {  
-  name: 'Google',   
- info: { employees: ['Vlad', 'Olga'], partners: ['Microsoft', 'Facebook', 'Xing']   } 
-};
+// const organisation = {  
+//   name: 'Google',   
+//  info: { employees: ['Vlad', 'Olga'], partners: ['Microsoft', 'Facebook', 'Xing']   } 
+// };
 
-function getInfo({name,info:{partners:[first,second]}}) {
+// function getInfo({name,info:{partners:[first,second]}}) {
 
-console.log(`Name: ${name}, Partners: ${first} ${second} `);
+// console.log(`Name: ${name}, Partners: ${first} ${second} `);
 
+// }
+
+// let org = getInfo(organisation);
+
+// Переделать функцию с использованием функции-стрелки (в методе reduce тоже использовать arrow function):
+
+// function sum() {
+//   const params = Array.prototype.slice.call(arguments);
+
+//   if (!params.length) return 0;
+
+//   return params.reduce(function (prev, next) { return prev + next; });
+// }
+
+// sum(1, 2, 3, 4); // 10
+// sum(); // 0
+
+const sum = (...args) => {
+  const params = Array.prototype.slice.call(args);
+  if (!params.length) return 0;
+
+  const prev_next = (prev, next) => prev + next;
+
+  return params.reduce(prev_next);
 }
 
-let org = getInfo(organisation);
+console.log(sum(1, 2, 3, 4));
+
+
+
 
 
 
