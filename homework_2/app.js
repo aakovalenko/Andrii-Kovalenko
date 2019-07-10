547e7af3d95a4e2e31d1583b0046a3805dfb0952
andrii <kaa89pl@gmail.com>
2019-07-10 15:18:48 +0300

    Destructuring task2

--- homework_2/app.js
+++ homework_2/app.js
@@ -1,24 +1,28 @@
-//Деструктурирующее присваивание
-// Используя rest оператор и деструктуризацию, создать функцию, которая принимает любое количество аргументов и возвращает объект, содержащий первый аргумент и массив из остатка:
+//2. Организовать функцию getInfo, которая принимает объект вида
+// { name: ...,  info: { employees: [...], partners: [ … ]  } }
+// и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две компании из массива partners:
 
-// func(‘a’, ‘b’, ‘c’, ‘d’) → 
-// {
-//   first: ‘a’,
-//   other: [‘b’, ‘c’, ‘d’]
-// }
+// const organisation = {  
+//   name: 'Google',   
+//   info: { employees: [‘Vlad’, ‘Olga’], partners: ['Microsoft', 'Facebook', 'Xing']   } 
+// };
+// getInfo(organisation); → 
+// Name: Google 
+// Partners: Microsoft Facebook
 
-function retObj() {
-  const abc = arguments;
+const organisation = {  
+  name: 'Google',   
+ info: { employees: ['Vlad', 'Olga'], partners: ['Microsoft', 'Facebook', 'Xing']   } 
+};
 
-  const [first, ...other ] = abc;
+function getInfo({name,info:{partners:[first,second]}}) {
 
-  let z = {first, other};
-  console.log(typeof z);
-  return z;
+console.log(`Name: ${name}, Partners: ${first} ${second} `);
+
 }
 
-let res2 = retObj('a','b','c','d');
-console.log(res2);
+let org = getInfo(organisation);
+
 
 
 
