70643c18c4ed5d15cc9de446508e9f1784765a96
andrii <kaa89pl@gmail.com>
2019-07-17 09:44:28 +0300

    task 2

--- homework_3/app.js
+++ homework_3/app.js
@@ -7,3 +7,19 @@ const rectangle = {
 }
 
 console.log(rectangle.getSquare());
+
+//2.
+const price = {
+  price: 10,
+  discount: '15%',
+  getPrice: function () {return this.price},
+  getPriceWithDiscount: function () {
+    // % from price
+    let percentFromPrice = (this.price * parseInt(this.discount)) / 100;
+    return this.price - percentFromPrice;
+  }
+
+ };
+
+ console.log(price.getPrice());
+ console.log(price.getPriceWithDiscount());
